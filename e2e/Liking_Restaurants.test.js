b3423cb6ae5bc3e1ec4bfa92b65dcccbb596d8dd
Yoga Tandean <yogagawoh01@gmail.com>
2023-12-01 21:18:23 +0700

    update final submission 3

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -1,6 +1,6 @@
 /* global Feature, Before, Scenario, locate */
 /* eslint no-undef: "error" */
-// const assert = require('assert')
+const assert = require('assert')
 
 Feature('Liking Restaurants')
 
@@ -8,60 +8,60 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite')
 })
 
-// Scenario('showing empty liked restaurants', ({ I }) => {
-//   I.seeElement('restaurant-list')
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('restaurant-list')
 
-//   I.see('No restaurants found', '.placeholder')
-// })
+  I.see('No restaurants found', '.placeholder')
+})
 
-// Scenario('liking one restaurant', async ({ I }) => {
-//   I.see('No restaurants found', '.placeholder')
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('No restaurants found', '.placeholder')
 
-//   I.amOnPage('/')
+  I.amOnPage('/')
 
-//   I.seeElement('restaurant-card')
-//   const firstRestaurant = locate('restaurant-card').first()
-//   const firstRestaurantTittle = await I.grabTextFrom(firstRestaurant)
-//   I.click(firstRestaurant)
+  I.seeElement('restaurant-card')
+  const firstRestaurant = locate('restaurant-card').first()
+  const firstRestaurantTittle = await I.grabTextFrom(firstRestaurant)
+  I.click(firstRestaurant)
 
-//   I.seeElement('#likeButton')
-//   I.click('#likeButton')
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
 
-//   I.amOnPage('/#/favorite')
-//   I.seeElement('restaurant-list')
-//   const likedRestaurantTitle = await I.grabTextFrom('restaurant-card')
+  I.amOnPage('/#/favorite')
+  I.seeElement('restaurant-list')
+  const likedRestaurantTitle = await I.grabTextFrom('restaurant-card')
 
-//   assert.strictEqual(firstRestaurantTittle, likedRestaurantTitle)
-// })
+  assert.strictEqual(firstRestaurantTittle, likedRestaurantTitle)
+})
 
-// Scenario('unliking one restaurant', async ({ I }) => {
-//   I.amOnPage('/')
+Scenario('unliking one restaurant', async ({ I }) => {
+  I.amOnPage('/')
 
-//   I.seeElement('restaurant-card')
-//   const firstRestaurant = locate('restaurant-card').first()
-//   await I.grabTextFrom(firstRestaurant)
-//   I.click(firstRestaurant)
+  I.seeElement('restaurant-card')
+  const firstRestaurant = locate('restaurant-card').first()
+  await I.grabTextFrom(firstRestaurant)
+  I.click(firstRestaurant)
 
-//   I.seeElement('#likeButton')
-//   I.click('#likeButton')
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
 
-//   I.amOnPage('/#/favorite')
-//   I.wait(2)
-//   I.seeElement('restaurant-card')
-//   const firstRestaurantOnFavoritePage = locate('restaurant-card').first()
-//   await I.grabTextFrom(firstRestaurantOnFavoritePage)
-//   I.click(firstRestaurantOnFavoritePage)
+  I.amOnPage('/#/favorite')
+  I.wait(2)
+  I.seeElement('restaurant-card')
+  const firstRestaurantOnFavoritePage = locate('restaurant-card').first()
+  await I.grabTextFrom(firstRestaurantOnFavoritePage)
+  I.click(firstRestaurantOnFavoritePage)
 
-//   I.seeElement('#likeButton')
-//   I.click('#likeButton')
-//   I.wait(2)
-//   I.amOnPage('/#/favorite')
-//   I.wait(2)
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
+  I.wait(2)
+  I.amOnPage('/#/favorite')
+  I.wait(2)
 
-//   I.seeElement('restaurant-list')
+  I.seeElement('restaurant-list')
 
-//   I.see('No restaurants found', '.placeholder')
-// })
+  I.see('No restaurants found', '.placeholder')
+})
 
 Scenario('go to page about us', ({ I }) => {
   I.amOnPage('/')
@@ -69,6 +69,6 @@ Scenario('go to page about us', ({ I }) => {
   I.wait(5)
   I.seeElement('.nav')
   I.click(locate('.nav').last())
-  I.wait(10)
+  I.wait(5)
   I.amOnPage('https://github.com/Tandeann')
 })
